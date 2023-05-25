fe7e73812ae999b37423abbfba1cf0ed15bcf81d
OMozes <olah.mozes@student.uni-miskolc.hu>
2023-05-25 22:41:28 +0200

    commit

--- Beadando/Program.cs
+++ Beadando/Program.cs
@@ -6,40 +6,40 @@ class Program
     static void Main()
     {
         DataCollection dataCollection = new DataCollection();
-        dataCollection.AddData("John", 25);
-        dataCollection.AddData("Alice", 30);
-        dataCollection.AddData("Bob", 40);
-        dataCollection.AddData("Sarah", 28);
-        dataCollection.AddData("David", 35);
-        dataCollection.AddData("Emma", 42);
-        dataCollection.AddData("Michael", 32);
-        dataCollection.AddData("Olivia", 38);
-        dataCollection.AddData("Daniel", 27);
-        dataCollection.AddData("Sophia", 33);
-        dataCollection.AddData("James", 41);
-        dataCollection.AddData("Emily", 29);
-        dataCollection.AddData("Jacob", 36);
-        dataCollection.AddData("Ava", 43);
-        dataCollection.AddData("Matthew", 31);
-        dataCollection.AddData("Isabella", 37);
-        dataCollection.AddData("Alexander", 26);
-        dataCollection.AddData("Mia", 34);
-        dataCollection.AddData("Ethan", 42);
-        dataCollection.AddData("Charlotte", 39);
+        dataCollection.AddData("Vivien", 25);
+        dataCollection.AddData("Viktória", 30);
+        dataCollection.AddData("Anna", 40);
+        dataCollection.AddData("Alexandra", 28);
+        dataCollection.AddData("Fanni", 35);
+        dataCollection.AddData("Dóra", 42);
+        dataCollection.AddData("Réka", 32);
+        dataCollection.AddData("Petra", 38);
+        dataCollection.AddData("Eszter", 27);
+        dataCollection.AddData("Nikolett", 33);
+        dataCollection.AddData("Dániel", 41);
+        dataCollection.AddData("Dávid", 29);
+        dataCollection.AddData("Bence", 36);
+        dataCollection.AddData("Máté", 43);
+        dataCollection.AddData("Tamás", 31);
+        dataCollection.AddData("Péter", 37);
+        dataCollection.AddData("Balázs", 26);
+        dataCollection.AddData("Ádám", 34);
+        dataCollection.AddData("Márk", 42);
+        dataCollection.AddData("Zoltán", 39);
 
-        Console.WriteLine("Original Data Collection:");
+        Console.WriteLine("Adatok:");
         dataCollection.PrintData();
 
-        Console.WriteLine("\n--- Data Collection Methods and Properties ---");
-        Console.WriteLine("Count: " + dataCollection.Count);
-        Console.WriteLine("Item1 of the first data: " + dataCollection.GetFirstItem());
-        Console.WriteLine("Contains data with name 'Alice': " + dataCollection.Contains("Alice"));
-        Console.WriteLine("Index of data with name 'Bob': " + dataCollection.IndexOf("Bob"));
-        Console.WriteLine("Last data: " + dataCollection.GetLastData());
+        Console.WriteLine("\n--- Metódusok és tulajdonságok ---");
+        Console.WriteLine("Adatok száma: " + dataCollection.Count);
+        Console.WriteLine("Az első név: " + dataCollection.GetFirstItem());
+        Console.WriteLine("Van-e 'Tamás nevű ember? " + dataCollection.Contains("Tamás"));
+        Console.WriteLine("'Nikolett' nevű ember indexe: " + dataCollection.IndexOf("Nikolett"));
+        Console.WriteLine("Utolsó adat: " + dataCollection.GetLastData());
 
-        Console.WriteLine("\n--- MemberwiseClone Example ---");
+        Console.WriteLine("\n--- MemberwiseClone példa ---");
         DataCollection cloneCollection = dataCollection.MemberwiseClone();
-        Console.WriteLine("Cloned Data Collection:");
+        Console.WriteLine("Másolt adatgyűjtemény:");
         cloneCollection.PrintData();
 
         Console.ReadLine();
