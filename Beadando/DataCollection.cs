bf0fa48db4d82890eda0848bb79d647a49146de0
OMozes <olah.mozes@student.uni-miskolc.hu>
2023-05-25 22:21:57 +0200

    commit

---
+++ Beadando/DataCollection.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+
+
+class DataCollection
+{
+	private List<Tuple<string, int>> data = new List<Tuple<string, int>>();
+
+	public int Count
+	{
+		get { return data.Count; }
+	}
+
+	public void AddData(string name, int age)
+	{
+		data.Add(Tuple.Create(name, age));
+	}
+
+	public string GetFirstItem()
+	{
+		if (data.Count > 0)
+		{
+			return data[0].Item1;
+		}
+		return null;
+	}
+
+	public bool Contains(string name)
+	{
+		foreach (var item in data)
+		{
+			if (item.Item1 == name)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int IndexOf(string name)
+	{
+		for (int i = 0; i < data.Count; i++)
+		{
+			if (data[i].Item1 == name)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public Tuple<string, int> GetLastData()
+	{
+		if (data.Count > 0)
+		{
+			return data[data.Count - 1];
+		}
+		return null;
+	}
+
+	public DataCollection MemberwiseClone()
+	{
+		DataCollection clone = new DataCollection();
+		foreach (var item in data)
+		{
+			clone.AddData(item.Item1, item.Item2);
+		}
+		return clone;
+	}
+
+	public void PrintData()
+	{
+		foreach (var item in data)
+		{
+			Console.WriteLine(item.Item1 + ", " + item.Item2);
+		}
+	}
+}
--- Beadando/Program.cs
+++ Beadando/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        DataCollection dataCollection = new DataCollection();
+        dataCollection.AddData("John", 25);
+        dataCollection.AddData("Alice", 30);
+        dataCollection.AddData("Bob", 40);
+        dataCollection.AddData("Sarah", 28);
+        dataCollection.AddData("David", 35);
+        dataCollection.AddData("Emma", 42);
+        dataCollection.AddData("Michael", 32);
+        dataCollection.AddData("Olivia", 38);
+        dataCollection.AddData("Daniel", 27);
+        dataCollection.AddData("Sophia", 33);
+        dataCollection.AddData("James", 41);
+        dataCollection.AddData("Emily", 29);
+        dataCollection.AddData("Jacob", 36);
+        dataCollection.AddData("Ava", 43);
+        dataCollection.AddData("Matthew", 31);
+        dataCollection.AddData("Isabella", 37);
+        dataCollection.AddData("Alexander", 26);
+        dataCollection.AddData("Mia", 34);
+        dataCollection.AddData("Ethan", 42);
+        dataCollection.AddData("Charlotte", 39);
+
+        Console.WriteLine("Original Data Collection:");
+        dataCollection.PrintData();
+
+        Console.WriteLine("\n--- Data Collection Methods and Properties ---");
+        Console.WriteLine("Count: " + dataCollection.Count);
+        Console.WriteLine("Item1 of the first data: " + dataCollection.GetFirstItem());
+        Console.WriteLine("Contains data with name 'Alice': " + dataCollection.Contains("Alice"));
+        Console.WriteLine("Index of data with name 'Bob': " + dataCollection.IndexOf("Bob"));
+        Console.WriteLine("Last data: " + dataCollection.GetLastData());
+
+        Console.WriteLine("\n--- MemberwiseClone Example ---");
+        DataCollection cloneCollection = dataCollection.MemberwiseClone();
+        Console.WriteLine("Cloned Data Collection:");
+        cloneCollection.PrintData();
+
+        Console.ReadLine();
+    }
+}
